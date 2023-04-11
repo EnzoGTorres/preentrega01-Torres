7621cea67972629488028fd553dfe0b71881956d
Enzo Torres <enzotorres462@gmail.com>
2023-04-10 21:25:14 -0300

    fist commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-let moneda = ""
-let monto = 0
-let cuotas = 0
-let interes = 0
-let totalInteres = 0.98
-let prestamo = 0
-let consulta = true
-let interesGarantia = 0
-let cotizacion = 0
-const valorDolar = 385
-const valorEuro = 400
+
 
 
 const eligeMoneda = "Ingresa la moneda de garantía \n" + "dolar \n" + "euro" 
@@ -33,17 +23,8 @@ function elegirMoneda() {
     }
 }
 
-function calculoPrestamo (moneda, interes, interesGarantia, cotizacion){
-    monto = prompt("elige el monto a depositar en :" + moneda)
-    prestamo = prompt("Elige el monto del prestamo en pesos")
-    cuotas = prompt ("Elige la cantidad de cuotas")
-            if (prestamo >= (monto * cotizacion) * interes)
-            alert(" No puedes pedir este monto en relacion a esa garantía.")
-        else {
-            alert("El valor de la cuota a pagar es: " + (((prestamo * (1 + parseFloat(interes/12)))/ cuotas) ))
-            alert("se guardarán tus " + monto + " " + moneda + " recibiendo un total de: " + monto * interesGarantia + " al finalizar el ultimo pago")
-    }
-}
+const nvoprestamo = new Prestamo(cantCuotas)
+alert('El total a pagar es de: $' +nvoprestamo.cantCuotas())
 
 function consultarPrestamo(){
     while(consulta) {
